ad8899300f17e3f3d2f50e1ab8eef4e4b89e8e55
math09 <91202900+math09@users.noreply.github.com>
2026-04-02 06:03:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ try {
     $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo 'Échec de la connexion : ' . $e->getMessage();
+    die('Échec de la connexion : ' . $e->getMessage());
 }
 
 // Envoi d'un message
